7572ac682e41091e0d9cdfdeff8d6a14c7a15061
Julien Picquet <dext.1234@gmail.com>
2020-08-10 21:56:07 +0200

    Ajout des différentes fonctions admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Campus;
+use App\Entity\Inscriptions;
+use App\Entity\Lieux;
 use App\Entity\Participants;
+use App\Entity\Sorties;
+use App\Entity\Villes;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
@@ -17,6 +21,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
+
         // redirect to some CRUD controller
         $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
 
@@ -28,9 +33,13 @@ class DashboardController extends AbstractDashboardController
         return [
             MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
 
-            MenuItem::section('Blog'),
-            MenuItem::linkToCrud('Categories', 'fa fa-tags', Campus::class),
-            MenuItem::linkToCrud('Participants', 'fa fa-file-text', Participants::class)
+            MenuItem::section('Gestion de la BDD'),
+            MenuItem::linkToCrud('Categories', 'fa fa-file-text', Campus::class),
+            MenuItem::linkToCrud('Participants', 'fa fa-file-text', Participants::class),
+            MenuItem::linkToCrud('Inscription', 'fa fa-file-text', Inscriptions::class),
+            MenuItem::linkToCrud('Lieux', 'fa fa-file-text', Lieux::class),
+            MenuItem::linkToCrud('Sorties', 'fa fa-file-text', Sorties::class),
+            MenuItem::linkToCrud('Villes', 'fa fa-file-text', Villes::class),
         ];
     }
 }
