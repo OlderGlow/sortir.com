04c9afd5f274e2433962c49ee3ee66c7e85041c2
Pierre Waflart <pierre.waflart@gmail.com>
2020-08-11 15:23:06 +0200

    Mise a jour AdminDashboardController

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Campus;
+use App\Entity\Participants;
 use App\Entity\Villes;
+use App\Form\CampusType;
+use App\Form\ParticipantsType;
 use App\Form\VillesType;
+use App\Repository\CampusRepository;
+use App\Repository\ParticipantsRepository;
 use App\Repository\VillesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -47,7 +53,7 @@ class AdminDashboardController extends AbstractController
     /**
      * @Route("/villes/ajouter", name="admin.ville.add")
      */
-    public function add(Request $request)
+    public function addVille(Request $request)
     {
         $villes = new Villes();
 
@@ -67,13 +73,13 @@ class AdminDashboardController extends AbstractController
         ]);
     }
 
-   /**
+    /**
      * @Route("/villes/{id}", name="admin.ville.edit")
      * @param Villes $villes
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function edit(Villes $villes, Request $request)
+    public function editVille(Villes $villes, Request $request)
     {
         $form = $this->createForm(VillesType::class, $villes);
         $form->handleRequest($request);
@@ -94,7 +100,7 @@ class AdminDashboardController extends AbstractController
 
 
     /**
-     * @Route("/villes/delete", name="admin.ville")
+     * @Route("/villes/{id}", name="admin.ville.delete")
      * @param Villes $villes
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
@@ -118,31 +124,191 @@ class AdminDashboardController extends AbstractController
 
 
     /*
-     * <------------------------SEPARATION PREMIERE ETAPE,
-     *                                                      NE PAS REGARDER LA SUITE ---------------------------------->
+     * <------------------------CAMPUS---------------------------------->
      */
 
 
 
 
     /**
-     * @Route("/campus", name="admin.campus")
+     * @Route("/campus", name="admin.campus.home")
      */
-    public function index()
+    public function homeCampus(PaginatorInterface $paginator, Request $request, CampusRepository $campusRepository)
     {
+
+        $user = $this->getUser();
+        // Affichage de la liste
+        $campus = $paginator->paginate(
+            $campusRepository->findAll(),
+            $request->query->get('page', 1),
+            5
+        );
+
+        return $this->render('admin_dashboard/index.html.twig', [
+            'campus' => $campus,
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/campus/ajouter", name="admin.campus.add")
+     */
+    public function addCampus(Request $request)
+    {
+        $campus = new Campus();
+
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($campus);
+            $this->em->flush();
+            $this->addFlash('success', 'Crée avec succès');
+            return $this->redirectToRoute('admin.campus.home');
+        }
+
+        return $this->render('admin_dashboard/add.html.twig', [
+            'campus' => $campus,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/campus/{id}", name="admin.campus.edit")
+     * @param Campus $campus
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editCampus(Campus $campus, Request $request)
+    {
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+            $this->addFlash('success', 'Bien modifié avec succès');
+            return $this->redirectToRoute('admin.ville.home');
+        }
+
+        return $this->render('admin_dashboard/edit.html.twig', [
+            'campus' => $campus,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/campus/{id}", name="admin.campus.delete")
+     * @param Campus $campus
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteCampus(Campus $campus, Request $request)
+    {
+        if($this->isCsrfTokenValid('delete' . $campus->getId(), $request->get('_token')))
+        {
+            $this->em->remove($campus);
+            $this->em->flush();
+            $this->addFlash('success', 'Supprimer avec succès');
+        }
+
         return $this->render('admin_dashboard/index.html.twig', [
             'controller_name' => 'AdminDashboardController',
         ]);
     }
 
-    /**
-     * @Route("/participants", name="admin.participants")
+    /*
+     * ------------------------------ PARTICIPANT-------------------------------
      */
-    /*public function index()
+
+    /**
+     * @Route("/participants", name="admin.participants.home")
+     */
+    public function homeParticipant(PaginatorInterface $paginator, Request $request, ParticipantsRepository $participantsRepository)
     {
+
+        $user = $this->getUser();
+        // Affichage de la liste
+        $participants = $paginator->paginate(
+            $participantsRepository->findAll(),
+            $request->query->get('page', 1),
+            5
+        );
+
+        return $this->render('admin_dashboard/index.html.twig', [
+            'participants' => $participants,
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/participant/ajouter", name="admin.participant.add")
+     */
+    public function addParticipant(Request $request)
+    {
+        $participant = new Participants();
+
+        $form = $this->createForm(ParticipantsType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($participant);
+            $this->em->flush();
+            $this->addFlash('success', 'Crée avec succès');
+            return $this->redirectToRoute('admin.participants.home');
+        }
+
+        return $this->render('admin_dashboard/add.html.twig', [
+            'campus' => $participant,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/participant/{id}", name="admin.participant.edit")
+     * @param Campus $campus
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editParticipant(Campus $campus, Request $request)
+    {
+        $form = $this->createForm(CampusType::class, $campus);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->em->flush();
+            $this->addFlash('success', 'Bien modifié avec succès');
+            return $this->redirectToRoute('admin.ville.home');
+        }
+
+        return $this->render('admin_dashboard/edit.html.twig', [
+            'campus' => $campus,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/participant/{id}", name="admin.participant.delete")
+     * @param Participants $participants
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteParticipant(Participants $participants, Request $request)
+    {
+        if($this->isCsrfTokenValid('delete' . $participants->getId(), $request->get('_token')))
+        {
+            $this->em->remove($participants);
+            $this->em->flush();
+            $this->addFlash('success', 'Supprimer avec succès');
+        }
+
         return $this->render('admin_dashboard/index.html.twig', [
             'controller_name' => 'AdminDashboardController',
         ]);
     }
-    */
 }
